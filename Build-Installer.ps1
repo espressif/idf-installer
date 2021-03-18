82dde9db08f8b01c69e97e477ec5de7ffe7895d4
Juraj Michalek <juraj.michalek@espressif.com>
2021-03-18 17:03:17 +0100

    bump up Curator version

--- Build-Installer.ps1
+++ Build-Installer.ps1
@@ -78,7 +78,7 @@ function PrepareIdfCurator {
     PrepareIdfPackage -BasePath build\$InstallerType\lib `
         -FilePath curator.exe `
         -DistZip idf-curator.zip `
-        -DownloadUrl https://github.com/georgik/idf-curator/releases/download/v0.0.3/idf-curator.zip
+        -DownloadUrl https://github.com/georgik/idf-curator/releases/download/v0.0.4/idf-curator.zip
 }
 
 function PrepareIdfGit {
