17eea15f3b1d453bd0debb27be7e5f426676b673
Juraj Michalek <juraj.michalek@espressif.com>
2022-04-05 13:18:27 +0200

    ci: fix reference to FrameworkSelector

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
-  <HelloWorld msg="Welcome to ESP-IDF Installer 3 (preview)"/>
+  <FrameworkSelector msg="Welcome to ESP-IDF Installer 3 (preview)"/>
 </template>
 
 <script>
