abd1bd2842ad9d2a75fc47d9690ad0950d3e4658
Kondal Kolipaka <kondal.kolipaka@espressif.com>
2022-08-18 13:24:22 +0530

    ci: Update builds with espressif-ide v2.6.0 and esp-idf v4.4.2 (#144)

--- Build-Installer.ps1
+++ Build-Installer.ps1
@@ -10,7 +10,7 @@ param (
     [ValidateSet('online', 'offline', 'espressif-ide')]
     $InstallerType = 'online',
     [String]
-    $OfflineBranch = 'v4.4',
+    $OfflineBranch = 'v4.4.2',
     [String]
     $Python = 'python',
     [Boolean]
@@ -20,7 +20,7 @@ param (
     [String]
     $IdfEnvVersion = '1.2.30',
     [String]
-    $EspressifIdeVersion = '2.5.0',
+    $EspressifIdeVersion = '2.6.0',
     [String]
     $JdkVersion = "jdk11.0.15_9",
     [String]
