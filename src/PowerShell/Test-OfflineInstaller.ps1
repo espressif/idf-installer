63edd709b7369ab055b380fd64bf17303d8468d7
Juraj Michálek <juraj.michalek@espressif.com>
2023-04-28 10:58:42 +0200

    ci: add file name to path

--- src/PowerShell/Test-OfflineInstaller.ps1
+++ src/PowerShell/Test-OfflineInstaller.ps1
@@ -11,7 +11,7 @@ param (
     [string]
     $InstallerPath = "build/esp-idf-installer.exe",
     [string]
-    $DestinationPath = "C:\Users\Public\"
+    $DestinationPath = "C:\Users\Public\esp-idf-installer.exe"
 )
 
 # Stop if any command fails
